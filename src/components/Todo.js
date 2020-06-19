d1292b83bac85bfeed6efe615134eaf8f909e0b2
eliHandy6 <zackogoma@gmail.com>
2020-06-20 00:14:33 +0300

    tod0list

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react"
+// import ReactDom from "react-dom"
+
+
+function Todo(){
+    return(
+        <div className="todo-item">
+             <p>Item to Strike out</p>
+             <input type="checkbox"/><br/>
+
+        </div>
+    )
+}
+
+export default Todo
